556a4679cfb4976d99bf481bd8fda4a50207a78b
ZeroRequiem126 <noboruonishi0126@gmail.com>
2022-02-28 14:46:02 +0900

    doneボタンを実装

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-const buttonList = document.querySelectorAll('button');
-    [...buttonList].forEach((button) => {
-        button.addEventListener('click', () => {
-            const li = button.closest('li');
-            li.classList.add('done');
-        })
+const button = document.getElementById('add-button');
+button.addEventListener('click', () => {
+    const input = document.getElementById('new-todo');
+
+    const li = document.createElement('li');
+    li.innerText = input.value;
+
+    const doneButton = document.createElement('button');
+    doneButton.innerText = 'done';
+    li.appendChild(doneButton);
+    doneButton.addEventListener('click', () => {
+        const li = doneButton.closest('li');
+        li.classList.add('done');
     });
+
+    const ul = document.querySelector('ul');
+    ul.appendChild(li);
+});
+
+
